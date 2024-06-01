59738a505e56d364167c1666973f9d4c30669247
Pranay Ghosh <pranayghosh@microsoft.com>
2024-06-01 10:25:45 +0530

    folder restructure

---
+++ Multi-Threading/LeetCode-Problems/src/FooBarAlternatively_1115/Semaphore/FooBar.java
@@ -0,0 +1,34 @@
+package FooBarAlternatively_1115.Semaphore;
+
+import java.util.concurrent.Semaphore;
+
+class FooBar {
+    private int n;
+    Semaphore foo, bar;
+
+    public FooBar(int n) {
+        this.n = n;
+        foo = new Semaphore(1);
+        bar = new Semaphore(0);
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            foo.acquire();
+            // printFoo.run() outputs "foo". Do not change or remove this line.
+            printFoo.run();
+            bar.release();
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            bar.acquire();
+            // printBar.run() outputs "bar". Do not change or remove this line.
+            printBar.run();
+            foo.release();
+        }
+    }
+}
